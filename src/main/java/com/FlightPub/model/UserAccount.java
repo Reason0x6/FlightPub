4317b32d65024d56a11a4a5d257b170afe3c409e
Reason6 <37852683+Reason0x6@users.noreply.github.com>
2022-05-06 13:50:30 +1000

    pwd included

--- src/main/java/com/FlightPub/model/UserAccount.java
+++ src/main/java/com/FlightPub/model/UserAccount.java
@@ -9,6 +9,8 @@ public class UserAccount {
 		private String id;
 		private String username;
 		private String email;
+
+		private String pwdHash;
 		
 		public UserAccount(String id, String username, String email) {
 			super();
@@ -32,13 +34,16 @@ public class UserAccount {
 		public void setUserName(String name) {
 			this.username = name;
 		}
+		public String getUserEmail() {
+		return this.email;
+	}
 
 		public void setUserEmail(String email) {
 			this.email = email;
 		}
 
-		public String getUserEmail() {
-		return this.email;
-	}
+		public void setPassword(String hash){ this.pwdHash = hash; }
+
+		public boolean checkPassword(String hash){ return pwdHash.equals(hash); }
 
 }
