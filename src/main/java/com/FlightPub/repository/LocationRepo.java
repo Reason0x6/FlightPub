5ebbf1d93940fe6d1649fed9b4f0f3ece3af09ec
William Paterson <c3280751@uon.edu.au>
2022-05-12 00:25:58 +1000

    Recommendation now uses a sort query rather than sorting in controller

--- src/main/java/com/FlightPub/repository/LocationRepo.java
+++ src/main/java/com/FlightPub/repository/LocationRepo.java
@@ -9,8 +9,8 @@ import org.springframework.data.mongodb.repository.Query;
 import java.util.List;
 
 public interface LocationRepo extends MongoRepository<Location, String> {
-    @Query(value="{ 'locationID' : {$ne : ?0} }")
-    List<Location> findAllExcluding(String dest);
+    @Query(value="{ 'locationID' : {$ne : ?0} }", sort = "{'popularity' : -1}")
+    List<Location> findAllSortedDescendingExcluding(String dest);
 
     @Query(value="{ 'location' : ?0 }")
     List<Location> findByLocation(String in);
