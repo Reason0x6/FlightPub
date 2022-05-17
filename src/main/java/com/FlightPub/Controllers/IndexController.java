06ab4fe28fab62b5c9f820b18c68d16d97767b84
Reason6 <37852683+Reason0x6@users.noreply.github.com>
2022-05-17 16:06:48 +1000

    groups page added

--- src/main/java/com/FlightPub/Controllers/IndexController.java
+++ src/main/java/com/FlightPub/Controllers/IndexController.java
@@ -126,11 +126,24 @@ public class IndexController {
 
     @RequestMapping("/account")
     public String account(Model model, HttpSession session){
-
+        if(!getSession(session).isLoggedIn()){
+            return "redirect:login";
+        }
         model.addAttribute("reco", getRecommendation());
         model.addAttribute("locs", locationServices.listAll());
         model.addAttribute("usr", getSession(session));
         return "Personalised";
+    }
+
+    @RequestMapping("/groups")
+    public String group(Model model, HttpSession session){
+        if(!getSession(session).isLoggedIn()){
+            return "redirect:login";
+        }
+        model.addAttribute("reco", getRecommendation());
+        model.addAttribute("locs", locationServices.listAll());
+        model.addAttribute("usr", getSession(session));
+        return "Group";
     }
 
     @PostMapping("/advancedSearch")
