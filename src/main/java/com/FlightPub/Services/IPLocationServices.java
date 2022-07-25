76b1a26bacedbfcd974e14b15bc7480e6e261439
SuzzwalShrestha <suzstz27@gmail.com>
2022-07-25 22:02:06 +1000

    IPLocationServices added.

---
+++ src/main/java/com/FlightPub/Services/IPLocationServices.java
@@ -0,0 +1,11 @@
+package com.FlightPub.Services;
+
+import com.FlightPub.model.Location;
+import com.FlightPub.repository.LocationRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+public class IPLocationServices {
+    /*Need To work*/
+
+}
