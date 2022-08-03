f57d6bdc44c3650e2259f5ad86bdd5585e97eb51
William Paterson <c3280751@uon.edu.au>
2022-08-03 23:58:52 +1000

    #FP-13 Recommendation location now uses the HTML geolocation api to find client location

--- src/main/java/com/FlightPub/Controllers/RecommendationController.java
+++ src/main/java/com/FlightPub/Controllers/RecommendationController.java
@@ -3,12 +3,16 @@ package com.FlightPub.Controllers;
 import com.FlightPub.RequestObjects.Recommendation;
 import com.FlightPub.Services.FlightServices;
 import com.FlightPub.Services.LocationServices;
+import com.FlightPub.model.Location;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Objects;
 
 @Controller
 public class RecommendationController {
@@ -29,7 +33,7 @@ public class RecommendationController {
     }
 
     @PostMapping("/recommendations")
-    public String loadRecommendations(@RequestParam("city") String city, Model model) {
+    public String loadRecommendations(@RequestParam String city, Model model) {
         recommendation = new Recommendation(locationServices, flightServices);
         recommendation.setRecommendationLocation(city);
 
@@ -38,5 +42,50 @@ public class RecommendationController {
         model.addAttribute("recommendationLocation", locationServices.listAll());
 
         return "Fragments/Recommendation :: recommendation_fragment";
+    }
+
+    @PostMapping("LocateNearestCity")
+    public String locateNearestCity(@RequestParam double lat, @RequestParam double lon, Model model) {
+        System.out.printf("Finding nearest city to lat: %f, lon: %f %n", lat, lon);
+
+        // Defaults for nearest city comparison
+        String currentNearestCity = "";
+        double currentSmallestDistance = 0;
+
+        // Loop though available locations and compare them to provided client lat and lon
+        for (Location location: locationServices.listAll()) {
+            // Distance between client lat/lon and location lat/lon
+            double tempDistance = distance(lat, lon, location.getLatitude(), location.getLongitude());
+
+            // Compare new distance to existing smallest distance
+            if(currentNearestCity.equals("") || tempDistance < currentSmallestDistance) {
+                currentNearestCity = location.getLocation();
+                currentSmallestDistance = tempDistance;
+            }
+        }
+
+        return loadRecommendations(currentNearestCity, model);
+    }
+
+    // --- Haversine formula for finding distances between two coordinates ---
+    private static final int EARTH_RADIUS = 6371; // Approx Earth radius in KM
+
+    public static double distance(double startLat, double startLong,
+                                  double endLat, double endLong) {
+
+        double dLat  = Math.toRadians((endLat - startLat));
+        double dLong = Math.toRadians((endLong - startLong));
+
+        startLat = Math.toRadians(startLat);
+        endLat   = Math.toRadians(endLat);
+
+        double a = haversine(dLat) + Math.cos(startLat) * Math.cos(endLat) * haversine(dLong);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return EARTH_RADIUS * c; // <-- d
+    }
+
+    private static double haversine(double val) {
+        return Math.pow(Math.sin(val / 2), 2);
     }
 }
