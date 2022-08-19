4dcc933c11d7dd7f6964ae743ecb056c52c9b3be
Gavin Austin <gavin.l.austin@uon.edu.au>
2022-08-19 22:36:57 +1000

    JS calc of selected seats

--- src/main/java/com/FlightPub/Controllers/IndexController.java
+++ src/main/java/com/FlightPub/Controllers/IndexController.java
@@ -386,6 +386,12 @@ public class IndexController {
         search.setCheapestPriceForStopOverResults(stopOver[1]);
         search.setCheapestPriceForStopOverResults(stopOver[2]);
 
+        System.out.println( flights[0].size());
+        System.out.println( flights[1].size());
+        System.out.println( stopOver[0].size());
+        System.out.println( stopOver[1].size());
+        System.out.println( stopOver[2].size());
+        model.addAttribute("count", flights[0].size()+flights[1].size()+stopOver[0].size()+stopOver[1].size()+stopOver[2].size());
 
         // Stops unnecessary objects from being added to the response
         if(flights[0] != null || flights[1] != null)
