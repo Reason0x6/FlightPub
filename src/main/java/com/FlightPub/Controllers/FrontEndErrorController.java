c8fa28f396db166bd4af250c67a8749d86c0564e
coderuntheworld <bella_andrews@me.com>
2022-08-24 20:27:10 +1000

    FP-49 Updated Comments in EmailController, FrontEndErrorController, GroupsController and RecommendationController

--- src/main/java/com/FlightPub/Controllers/FrontEndErrorController.java
+++ src/main/java/com/FlightPub/Controllers/FrontEndErrorController.java
@@ -8,11 +8,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
 
+/**
+ * Implements error handling functionality
+ */
 @Controller
 public class FrontEndErrorController implements ErrorController {
 
     private static final String PATH = "/error";
 
+    /**
+     * Returns a html fragment containing error message
+     *
+     * @param model   interface that defines a holder for model attributes
+     * @param session current session
+     * @return html error fragment
+     */
     @RequestMapping(value = PATH)
     public String myerror(Model model, HttpSession session) {
         model.addAttribute("usr", getSession(session));
@@ -20,12 +30,20 @@ public class FrontEndErrorController implements ErrorController {
         return "Error/404";
     }
 
+    /**
+     * Returns the path of the error page
+     * @return path of the error page
+     */
     public String getErrorPath(Model model, HttpSession session) {
         model.addAttribute("usr", getSession(session));
         model.addAttribute("Error", "General Error");
         return "404";
     }
 
+    /**
+     * Returns the session from the current request
+     * @return session from the current request
+     */
     private UserSession getSession(HttpSession session) {
         UserSession sessionUser = null;
         try {
